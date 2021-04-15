06874023dd21ce9dff630ac7e5865610a2f88c07
jayesh <jayeshkale052@gmail.com>
2021-04-15 19:14:03 +0530

    search functinality work

--- news.js
+++ news.js
@@ -1,22 +1,96 @@
 
 
 
-  var small_nav=document.getElementById("small_nav")
-  var img=document.getElementById("img")
-  var heading=document.getElementById("heading")
-  var small_heading=document.getElementById("small_heading")
-  var date=document.getElementById("date")
+ 
 
-
+///  getting data .
+   var data_cont=document.getElementById("showdata")
+   
 
   async function data(){
-      var responce=await fetch('https://www.espncricinfo.com/ci/content/story/data/index.json?;type=7;page=1')
-      var collect=await responce.json()
-      console.log(collect)
+      for( var i=1;i<=10;i++){
+        var responce=await fetch(`https://www.espncricinfo.com/ci/content/story/data/index.json?;type=7;page=${i}`)
+        var collect=await responce.json()
+    
 
-     collect.forEach(element => {
-           var p=document.createElement("p")
-           p.innerHTML=`${element.}`
-      });
-  }
+      
+       collect.forEach(element => {
+        
+        
+            var div=document.createElement("div")
+            div.innerHTML=`  <div id="news_data">
+            <div id="data_contain">
+            <p>${element.author}</p>
+            <div class="img">
+                <img   src="${element.image_path}" width="280px" height="200" alt="">
+            </div>
+            <div class="data">
+                <a href="#" style="text-decoration: none; color: black;"><h1>${element.headline}</h1></a>
+                <p><h3> ${element.date} </h3>${element.summary}</p>
+            </div>
+        </div>
+    </div>
+    <div style="clear: both;"></div>`
+  
+    data_cont.append(div)
+        });
+    }
+      }
+      
   data()
+
+
+
+//    search functinality
+
+ var input_value=document.getElementById("search_data")
+
+
+input_value.addEventListener("keyup",function(e){
+   
+    {
+        var grabvalue=input_value.value
+        // console.log(grabvalue)
+        data_cont.innerHTML=null;
+
+        
+        async function data(){
+            for( var i=1;i<=10;i++){
+              var responce=await fetch(`https://www.espncricinfo.com/ci/content/story/data/index.json?;type=7;page=${i}`)
+               var collect=await responce.json()
+               
+            //    data_cont.innerHTML=null
+            //    console.log(collect)
+
+            collect.forEach(element => {
+               
+                if(element.headline.toLowerCase().includes(grabvalue) ){
+                    var div=document.createElement("div")
+                    div.innerHTML=`  <div id="news_data">
+                    <div id="data_contain">
+                    <p>${element.author}</p>
+                    <div class="img">
+                        <img   src="${element.image_path}" width="280px" height="200" alt="">
+                    </div>
+                    <div class="data">
+                        <a href="#" style="text-decoration: none; color: black;"><h1>${element.headline}</h1></a>
+                        <p><h3> ${element.date} </h3>${element.summary}</p>
+                    </div>
+                </div>
+            </div>
+            <div style="clear: both;"></div>`
+          
+            data_cont.append(div)
+       
+                }
+
+        
+                       });
+
+            
+            }
+        }
+        data()
+       
+}
+})
