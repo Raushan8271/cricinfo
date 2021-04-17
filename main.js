c1641505c021ef503f96e2989f2326fdc82efc00
Raushan Kumar <36612181+Raushan8271@users.noreply.github.com>
2021-04-17 22:26:32 +0530

    footer

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 import { headerSecond } from "./component/headerSecond.js"
 import { rightContent } from "./component/rightContent.js"
+import { footer } from "./component/footer.js"
 document.getElementById('headerSec').append(headerSecond())
 document.getElementById('right').append(rightContent())
+document.getElementById('footer').append(footer())
+
+
+
+
+
+async function score() {
+    try {
+        let req = await fetch('https://cricapi.com/api/cricketScore/?apikey=StC74k5A68dv1fOjQvPXJDVGkeS2&unique_id=1251578')
+        let data = await req.json()
+            //console.log(data)
+    } catch (e) {
+        console.log(e)
+    }
+}
+score()
